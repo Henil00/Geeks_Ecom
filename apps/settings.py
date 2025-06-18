dda56437ce8a9cf853e9d0546350b45f155fbe4f
Bhumi <gohilbhumi2004@gmail.com>
2025-06-18 17:40:18 +0530

    changed userprofile

--- apps/settings.py
+++ apps/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'stockmovement',
     'payment',
     'product',
+    'profileuser',
 ]
 
 MIDDLEWARE = [
--- profileuser/admin.py
+++ profileuser/admin.py
@@ -1,9 +1,16 @@
 
-
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from .models import UserProfile
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'phone']
-    
+User = get_user_model()
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+class CustomUserAdmin(admin.ModelAdmin):
+    inlines = [UserProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
