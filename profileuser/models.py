d4f1c10cdb938450cea21bce0193e79c93712e1d
Bhumi <gohilbhumi2004@gmail.com>
2025-06-18 16:50:39 +0530

    added profileuser

---
+++ profileuser/models.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
